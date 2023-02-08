7c1a820ca3141f98cdaeb24718b2fa39500a2008
Mike Turner <80823747+NephronTurner@users.noreply.github.com>
2023-02-08 10:12:12 +0000

    add chr2 scripts

--- 06_haplotype-analyses/02_IMPUTE5-analyses-and-SVD/02.02q.MT_IMPUTE5_60K_Chr_2q.sh
+++ 06_haplotype-analyses/02_IMPUTE5-analyses-and-SVD/02.02q.MT_IMPUTE5_60K_Chr_2q.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #$ -cwd
 #$ -P emberson.prjc
-#$ -N MT_IMPUTE5_MCPS_60K_chr1pii
+#$ -N MT_IMPUTE5_MCPS_60K_chr2qii
 #$ -q long.qc
 #$ -pe shmem 44
-#$ -o ./code/IMPUTE5/60K_analyses/logs/MT_IMPUTE5_60K_1pii.out
-#$ -e ./code/IMPUTE5/60K_analyses/logs/MT_IMPUTE5_60K_1pii.err
+#$ -o ./code/IMPUTE5/60K_analyses/logs/MT_IMPUTE5_60K_2qii.out
+#$ -e ./code/IMPUTE5/60K_analyses/logs/MT_IMPUTE5_60K_2qii.err
 
 echo "-----------------------------------------"
 echo "Run on host: "`hostname`
@@ -14,7 +14,7 @@ echo "Username: "`whoami`
 echo "Started at:"`date`
 echo "-----------------------------------------"
 
-	##This script tries to run IMPUTE5 on chr1p to create a co-ancestry matrix##
+	##This script tries to run IMPUTE5 on chr 2q to create a co-ancestry matrix##
 	##The separate matrices will subsequently be summed then standardised##
 
 module load BCFtools/1.10.2-GCC-8.3.0
@@ -24,14 +24,14 @@ map_folder=./pop_structure/input_data
 ref_folder=./data/QC_datasets/60K_files/Phased_Chr_files/Imp5Files
 input_folder=./data/QC_datasets/60K_files/Phased_Chr_files/
 output_folder=./data/IMPUTE5_Output/60K_analyses
-chr=1
+chr=2
 
-	#Running IMPUTE5 to create co-ancestry matrix with all 60K participants#
+	#Running IMPUTE5 to create co-ancestry matrix with all 138K participants#
 
 $IMPUTE5_folder/impute5_1.1.4_static --h $ref_folder/MT_MCPS_60K_chr$chr.imp5 \
 --m $map_folder/chr$chr.b38.gmap.gz \
---g $input_folder/MT_MCPS_60K_phased_MAF1pc_no_ambigs_chr$chr.vcf.gz --r $chr:1-130000000 \
---o $output_folder/IMPUTED_chunks/MT_MCPS_60K_IMPUTE5_output_Chr$chr.p.vcf.gz \
---ohapcopy $output_folder/Shared_genome_matrices/MT_MCPS_60K_hapcopy_Chr$chr.p.shared \
+--g $input_folder/MT_MCPS_60K_phased_MAF1pc_no_ambigs_chr$chr.vcf.gz --r $chr:90000001-242200000 \
+--o $output_folder/IMPUTED_chunks/MT_MCPS_60K_IMPUTE5_output_Chr$chr.q.vcf.gz \
+--ohapcopy $output_folder/Shared_genome_matrices/MT_MCPS_60K_hapcopy_Chr$chr.q.shared \
 --ban-repeated-sample-names
 
